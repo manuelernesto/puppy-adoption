b8d15845f47bc541ae290343a65f658cf9ea2862
manuelernesto <manuelernesto@outlook.com>
2021-02-25 02:20:53 +0100

    first commit

---
+++ app/src/main/java/io/github/manuelernesto/puppyadoption/MainActivity.kt
@@ -0,0 +1,38 @@
+package io.github.manuelernesto.puppyadoption
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.manuelernesto.puppyadoption.ui.theme.PuppyAdoptionTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PuppyAdoptionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PuppyAdoptionTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/io/github/manuelernesto/puppyadoption/ui/component/PuppyListItem.kt
@@ -0,0 +1,2 @@
+package io.github.manuelernesto.puppyadoption.ui.component
+
